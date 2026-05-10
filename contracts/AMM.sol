6ec6c8209bd7cdb2eb4a67be5764d9312070f379
Brett Seibert <brs5241psu@gmail.com>
2026-05-10 19:44:32 -0400

    Swapping Tokens

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -15,6 +15,8 @@ contract AMM {
     mapping(address => uint256) public shares;
     uint256 constant PRECISION = 10**18;
 
+    event Swap(address user, address tokenGive, uint256 tokenGiveAmount, address tokenGet, uint256 tokenGetAmount, uint256 token1Balance, uint256 token2Balance, uint256 timestamp);
+
     constructor(Token _token1, Token _token2) {
         token1= _token1;
         token2= _token2;
@@ -24,15 +26,13 @@ contract AMM {
         require(token1.transferFrom(msg.sender, address(this), _token1Amount), "Failed to Transfer Token 1");
         require(token2.transferFrom(msg.sender, address(this), _token2Amount), "Failed to Transfer Token 2");
 
-       
-
         uint256 share;
         if (totalShares == 0) { 
             share = 100 * PRECISION;
         } else {
             uint256 share1 = (totalShares * _token1Amount) / token1Balance;
             uint256 share2 = (totalShares * _token2Amount) / token2Balance;
-            require((share1 /1 *10**3 ) == share2, "Must Provide Equal Token Amounts");
+            require((share1 / 10**3 ) == (share2 / 10**3), "Must Provide Equal Token Amounts");
             share = share1;
         }
 
@@ -50,5 +50,51 @@ contract AMM {
 
     function calculateToken1Deposit(uint256 _token2Amount) public view returns (uint256 token1Amount) {
         token1Amount = (token1Balance * _token2Amount) / token2Balance;
+    }
+
+    function calculateToken1Swap(uint256 _token1Amount) public view returns (uint256 token2Amount) {
+        uint256 token1After = token1Balance + _token1Amount;
+        uint token2after = K / token1After;
+        token2Amount = token2Balance -token2after;
+
+        if(token2Amount == token2Balance) {
+            token2Amount --;
+        }
+
+        require(token2Amount < token2Balance, "Swap Cannot Exceed Poll Balance");
+    }
+
+    function calculateToken2Swap(uint256 _token2Amount) public view returns (uint256 token1Amount) {
+        uint256 token2After = token2Balance + _token2Amount;
+        uint token1after = K / token2After;
+        token1Amount = token1Balance -token1after;
+
+        if(token1Amount == token1Balance) {
+            token1Amount --;
+        }
+
+        require(token1Amount < token1Balance, "Swap Cannot Exceed Poll Balance");
+    }
+
+    function swapToken1(uint256 _token1Amount) external returns (uint256 token2Amount) {
+        token2Amount = calculateToken1Swap(_token1Amount);
+
+        token1.transferFrom(msg.sender, address(this), _token1Amount);
+        token1Balance += _token1Amount;
+        token2Balance -= token2Amount;
+        token2.transfer(msg.sender, token2Amount);
+
+        emit Swap(msg.sender, address(token1), _token1Amount, address(token2), token2Amount, token1Balance, token2Balance, block.timestamp);
+    }
+
+    function swapToken2(uint256 _token2Amount) external returns (uint256 token1Amount) {
+        token1Amount = calculateToken2Swap(_token2Amount);
+
+        token2.transferFrom(msg.sender, address(this), _token2Amount);
+        token2Balance += _token2Amount;
+        token1Balance -= token1Amount;
+        token1.transfer(msg.sender, token1Amount);
+
+        emit Swap(msg.sender, address(token2), _token2Amount, address(token1), token1Amount, token1Balance, token2Balance, block.timestamp);
     }
 }
