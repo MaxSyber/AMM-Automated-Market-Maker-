ed945d7b91128ebc10b8a6e0c698fb9994d1bdc1
Brett Seibert <brs5241psu@gmail.com>
2026-05-10 17:08:06 -0400

    Add Liquidity

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -8,9 +8,47 @@ contract AMM {
     Token public token1;
     Token public token2;
 
+    uint256 public token1Balance;
+    uint256 public token2Balance;
+    uint256 public K;
+    uint256 public totalShares;
+    mapping(address => uint256) public shares;
+    uint256 constant PRECISION = 10**18;
+
     constructor(Token _token1, Token _token2) {
         token1= _token1;
         token2= _token2;
-        
+
+    }
+    function addLiquidity(uint256 _token1Amount, uint256 _token2Amount) external {
+        require(token1.transferFrom(msg.sender, address(this), _token1Amount), "Failed to Transfer Token 1");
+        require(token2.transferFrom(msg.sender, address(this), _token2Amount), "Failed to Transfer Token 2");
+
+       
+
+        uint256 share;
+        if (totalShares == 0) { 
+            share = 100 * PRECISION;
+        } else {
+            uint256 share1 = (totalShares * _token1Amount) / token1Balance;
+            uint256 share2 = (totalShares * _token2Amount) / token2Balance;
+            require((share1 /1 *10**3 ) == share2, "Must Provide Equal Token Amounts");
+            share = share1;
+        }
+
+        token1Balance += _token1Amount;
+        token2Balance += _token2Amount;
+        K = token1Balance * token2Balance;
+
+        totalShares += share;
+        shares[msg.sender] += share;
+    }
+
+    function calculateToken2Deposit(uint256 _token1Amount) public view returns (uint256 token2Amount) {
+        token2Amount = (token2Balance * _token1Amount) / token1Balance;
+    }
+
+    function calculateToken1Deposit(uint256 _token2Amount) public view returns (uint256 token1Amount) {
+        token1Amount = (token1Balance * _token2Amount) / token2Balance;
     }
 }
